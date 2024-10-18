1a3b39ac065bea9d78a8033b8b2a41edc716542f
antoniorusso2 <russoantonio303@outlook.it>
2024-10-18 17:05:45 +0200

    ADD: second script for a 'Odd or Even' game

---
+++ javascript/pari-e-dispari.js
@@ -0,0 +1,79 @@
+//2.Pari e Dispari
+
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+//scelta tra pari o dispari
+  //prompt
+
+const oddOrEven = prompt('Scegli \'pari o dispari\'').toLowerCase();//string --- 'pari' ||'dispari'
+console.log(oddOrEven);
+
+  //validazione pari o dispari
+    //SE parola inserita !== da pari o dispari
+      //-alert e ricarica pagina
+
+//scelta numero da parte dell'utente da 1 a 5
+  //prompt
+const nUser = Math.round(prompt('inserisci un numero da 1 a 5'));//number
+console.log(typeof nUser, nUser)
+  //condizione- SE numero maggiore di 5 O minore di 1
+  if (nUser < 1 || nUser > 5){
+    //-alert e ricarica pagina
+    alert('Il numero inserito non è corretto, ricaricare la pagina per ricominciare');
+  }
+    
+//generare numero random da 1 a 5 per il computer (funzione)
+function randomIntBetween(min, max) {
+  //math.random e math.floor per la generazione di un numero da 1 a 5
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const nCpu = randomIntBetween(1, 5);
+console.log(nCpu)
+  
+//somma numeri
+let sum = nCpu + nUser;
+console.log(nUser, nCpu, sumOddOrEven);
+
+//funzione verifica numero pari o dispari
+//funzione con calcolo numero % 2 => resto === 0
+//stabilire se la somma dei due numeri sia pari o dispari
+function isEven(num){
+  let even = num % 2; // 0 - 1
+  console.log(even)
+  if(even === 0){
+    return true
+  } else {
+    return false
+  }
+}
+
+let sumOddOrEven = isEven(sum);
+
+//SE l'utente ha scelto peri o dispari e la somma e' uguale alla scelta dell'utente
+if(oddOrEven === 'pari' && sumOddOrEven === true){
+  console.log('hai vinto');
+} else if (oddOrEven === 'dispari' && sumOddOrEven === false){
+  console.log('hai vinto');
+} else {
+  console.log('hai perso');
+}
+  //-l'utente vince
+//altrimenti perde
+
+
+
+
+
+
+  
+//dichiarare un vincitore
+  //-SE la somma dei due numeri e' uguale al valore 'pari o dispari' scelto dall'utente allora l'utente ha vinto
+  //-ALTRIMENTI ha perso
+
+// function winnerIs(a, b){
+//   if(oddOrEven === )
+
+// }
+  //Maggiore === vincitore
+

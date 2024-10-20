4ded54bcceace09cff4d4ccfdd3c69cf941ea568
antoniorusso2 <russoantonio303@outlook.it>
2024-10-20 18:00:02 +0200

    -removed all the code nested in the else and modified the condition for validate the user input about choosing 'pari o dispari';

--- javascript/pari-e-dispari.js
+++ javascript/pari-e-dispari.js
@@ -8,51 +8,54 @@
 const userChoice = prompt('Scegli \'pari o dispari\'').toLowerCase();//string --- 'pari' ||'dispari'
 console.log(userChoice);
 
-//validazione pari o dispari
-if(userChoice  === 'pari' || userChoice === 'dispari'){
-
+  //validazione pari o dispari
+  if(userChoice  !== 'pari' && userChoice !== 'dispari'){
+    alert('la scelta effettuata non è consentita. \n Ricaricare la pagina e scegliere tra \"pari\" o \"dispari\"');
+    location.reload(); 
+    
+  }
   //scelta numero da parte dell'utente da 1 a 5
   //prompt
-  const nUser = Math.round(prompt('inserisci un numero da 1 a 5'));//number
+const nUser = Math.round(prompt('inserisci un numero da 1 a 5'));//number
 
-  console.log(`il numero che hai scelto è: ${nUser}`)
+console.log(`il numero che hai scelto è: ${nUser}`)
 
   //condizione- SE numero maggiore di 5 O minore di 1
   if (nUser < 1 || nUser > 5){
     //-alert e ricarica pagina
     alert('Il numero inserito non è corretto, ricaricare la pagina per ricominciare');
-    location.reload()
+    location.reload();
   }
-    
-  //generare numero random da 1 a 5 per il computer (funzione)
-  function randomIntBetween(min, max) {
-    //math.random e math.floor per la generazione di un numero da 1 a 5
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  
+//generare numero random da 1 a 5 per il computer (funzione)
+function randomIntBetween(min, max) {
+  //math.random e math.floor per la generazione di un numero da 1 a 5
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const nCpu = randomIntBetween(1, 5);
+console.log(`numero CPU: ${nCpu}`);
+  
+//funzione verifica numero pari o dispari
+//funzione con calcolo numero % 2 => resto === 0
+//stabilire se la somma dei due numeri sia pari o dispari
+function isEven(num){
+  let even = num % 2; // 0 - 1
+
+  // console.log(even)
+
+  if(even === 0){
+    return true
+  } else {
+    return false
   }
+}
 
-  const nCpu = randomIntBetween(1, 5);
-  console.log(`numero CPU: ${nCpu}`)
-    
-  //funzione verifica numero pari o dispari
-  //funzione con calcolo numero % 2 => resto === 0
-  //stabilire se la somma dei due numeri sia pari o dispari
-  function isEven(num){
-    let even = num % 2; // 0 - 1
+let sum = nCpu + nUser; // somma numeri
+let sumOddOrEven = isEven(sum); //booleano per confronto con input utente
+console.log(`la somma dei due numeri è ${sum}`);
 
-    // console.log(even)
-
-    if(even === 0){
-      return true
-    } else {
-      return false
-    }
-  }
-
-  let sum = nCpu + nUser; // somma numeri
-  let sumOddOrEven = isEven(sum); //booleano per confronto con input utente
-  console.log(`la somma dei due numeri è ${sum}`);
-
-  //SE l'utente ha scelto peri o dispari e la somma e' uguale alla scelta dell'utente
+//SE l'utente ha scelto peri o dispari e la somma e' uguale alla scelta dell'utente
   if(userChoice === 'pari' && sumOddOrEven === true){
     console.log('hai vinto');
   } else if (userChoice === 'dispari' && sumOddOrEven === false){
@@ -61,10 +64,5 @@ if(userChoice  === 'pari' || userChoice === 'dispari'){
     console.log('hai perso');
   }
   //-l'utente vince
-  //altrimenti perde
+    //altrimenti perde
       
-} else {
-  alert('la scelta effettuata non è consentita. \n Ricaricare la pagina e scegliere tra \"pari\" o \"dispari\"');
-  location.reload(); 
-}
-
